2993002e146e8a381edb7b7bd1ec4cf75c0bed90
thamilton <thamilton@scout.local>
2016-09-06 19:58:21 -0400

    Added some updates to users and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
 
-  get 'static_pages/help'
+  get "/home" => "static_pages#home"
+  get "/help" => "static_pages#help"
+  get "/about" => "static_pages#about"
 
-  get 'static_pages/about'
+  get  '/signup',  to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
