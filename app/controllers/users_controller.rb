96d1293d06e4504a690e9017c49b9d17b6867d9f
thamilton <thamilton@scout.local>
2016-09-07 23:49:45 -0400

    Added updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
   def new
     @user = User.new
   end
 
   def index
-    @users = User.where(activated: Time.zone.now).paginate(page: params[:page])
   end
 
   def show
@@ -23,9 +26,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
 private
@@ -34,4 +51,25 @@ private
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
 end
